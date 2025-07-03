271d885660b5974d97cbf916e3f90836852aa530
Mudit Mathur <mudit.manu.mathur@gmail.com>
2025-07-03 16:05:15 +0530

    final clean up before sharing

--- apps/server/src/configs/configs.ts
+++ apps/server/src/configs/configs.ts
@@ -4,6 +4,7 @@ dotenv.config();
 class Configs{
 
     public static PORT:string = process.env.PORT || "4000";
+    public static FRONTEND_URL = process.env.FRONTEND_URL || "";
     public static WOOLF_GEMIN_ENDPOINT:string = process.env.WOOLF_GEMIN_ENDPOINT || "";
     public static WOOLF_GEMINI_TOKEN:string = process.env.WOOLF_GEMINI_TOKEN || "";
     public static GEMINI_API_KEY:string = process.env.GEMINI_API_KEY || "";
